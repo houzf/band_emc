a15124c6833a5f1c30f9349ec7b9ccd56843f602
Zhufeng Hou <hou.zhufeng@gmail.com>
2021-05-17 18:33:06 +0800

    Add files via upload
    the log of fit for each direction is separately saved. the format of summary file for the fitting results is adjusted.

--- template-gnuplot-scripts/plot-ke4fit.sh
+++ template-gnuplot-scripts/plot-ke4fit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ ! -f ke4fit.dat ] ; then  
+if [ ! -f ke4fit.dat ] ; then
 	echo "ke4fit.dat doesn't exist!"
 	exit
 fi
@@ -8,7 +8,7 @@ if [ -f inp.em ]; then
 	nk=`cat inp.em | tail -1 | awk '{printf "%d\n", 2 * $2 + 1 }' `
 	echo $nk
 else
-	echo "How many k points along each directions? Please enter an integer number" 
+	echo "How many k points along each directions? Please enter an integer number"
 	read nk
 fi
 
@@ -44,15 +44,15 @@ do
 fstl=`echo "scale=0; (($i-1) * $nk +  $i) " | bc `
 lstl=`echo "scale=0; ($i * $nk + $i - 1) " | bc  `
 echo $fstl   $lstl
-sed -n "$fstl","$lstl"p  ke4fit.dat  > ke4fit-$dirt.dat 
-rm -f fit.log
+sed -n "$fstl","$lstl"p  ke4fit.dat  > ke4fit-$dirt.dat
+#rm -f fit.log
 cat > plot-VB-ke-fit.plt <<!
 
 reset
 
 set encoding iso_8859_1
 set terminal postscript eps enhanced color  "Times-Roman" 35
-set output "VB-ke-$dirt-fitted.eps"     # name of output file
+set output "VB-ke-$dirt-fit.eps"     # name of output file
 
 # Color definitions
 set border linewidth 3.5
@@ -78,7 +78,7 @@ set style line 3  linetype 1 linecolor rgb "blue"    linewidth 8.000 pointtype 1
 set style line 33 linetype 2 linecolor rgb "blue"    linewidth 5.000 pointtype 3 pointsize default
 set style line 4  linetype 1 linecolor rgb "magenta"    linewidth 6.000 pointtype 1 pointsize default
 set style line 44 linetype 2 linecolor rgb "magenta"    linewidth 5.000 pointtype 3 pointsize default
-#set key at 5.2,1.5  noreverse enhanced box linetype -2 linewidth 1.200 samplen 2 spacing 1.5 width 0.0 height 0  
+#set key at 5.2,1.5  noreverse enhanced box linetype -2 linewidth 1.200 samplen 2 spacing 1.5 width 0.0 height 0
 set key right  top Right noreverse enhanced box linetype -2 linewidth 1.200 samplen 2 spacing 1.5 width 0.0 height 0  autotitles
 set xlabel 'K-K_0 [1/Bohr]' offset 0,0.5
 set ylabel 'Eigenvalue [Ha]' offset 1,0
@@ -90,10 +90,11 @@ set format x "%5.3f"
 set xtics rotate by -45
 #FIT_LIMIT= 1e-6
 #set fit quiet
+set fit logfile "$dirt-VB-fit.log"
 f1(x)= a * x**2 + b * x + c
-fit f1(x) "ke4fit-$dirt.dat" using 1:2 via a, b, c 
+fit f1(x) "ke4fit-$dirt.dat" using 1:2 via a, b, c
 f2(x)= p * x**2 + q * x + r
-fit f2(x) "ke4fit-$dirt.dat" using 1:3 via p, q, r 
+fit f2(x) "ke4fit-$dirt.dat" using 1:3 via p, q, r
 f3(x)= u * x**2 + v * x + w
 fit f3(x) "ke4fit-$dirt.dat" using 1:4 via u, v, w
 #set datafile separator ","
@@ -102,22 +103,23 @@ plot f1(x) with lines ls 2  notitle '',\\
      f2(x) with lines ls 3 notitle '', \\
      "ke4fit-$dirt.dat" u 1:3   with points  lt 3 lw 4 pt 6 ps 4 notitle '' ,\\
      f3(x) with lines ls 4 notitle '', \\
-     "ke4fit-$dirt.dat" u 1:4   with points  lt 4 lw 4 pt 8 ps 4 notitle '' 
+     "ke4fit-$dirt.dat" u 1:4   with points  lt 4 lw 4 pt 8 ps 4 notitle ''
 !
 /usr/bin/gnuplot  plot-VB-ke-fit.plt
 
-a=`grep 'a               = ' fit.log | tail -1 | awk '{printf "%12.6f\n", $3}'`
-p=`grep 'p               = ' fit.log | tail -1 | awk '{printf "%12.6f\n", $3}'`
-u=`grep 'u               = ' fit.log | tail -1 | awk '{printf "%12.6f\n", $3}'`
+a=`grep 'a      ' "$dirt"-VB-fit.log |grep '='| tail -1 | awk '{printf "%12.6f\n", $3}'`
+p=`grep 'p      ' "$dirt"-VB-fit.log |grep '='| tail -1 | awk '{printf "%12.6f\n", $3}'`
+u=`grep 'u      ' "$dirt"-VB-fit.log |grep '='| tail -1 | awk '{printf "%12.6f\n", $3}'`
 
-rm -f fit.log
+#rm -f fit.log
+
 cat > plot-CB-ke-fit.plt <<!
 
 reset
 
 set encoding iso_8859_1
 set terminal postscript eps enhanced color  "Times-Roman" 35
-set output "CB-ke-$dirt-fitted.eps"     # name of output file
+set output "CB-ke-$dirt-fit.eps"     # name of output file
 
 # Color definitions
 set border linewidth 3.5
@@ -143,7 +145,7 @@ set style line 3  linetype 1 linecolor rgb "blue"    linewidth 8.000 pointtype 1
 set style line 33 linetype 2 linecolor rgb "blue"    linewidth 5.000 pointtype 3 pointsize default
 set style line 4  linetype 1 linecolor rgb "magenta"    linewidth 6.000 pointtype 1 pointsize default
 set style line 44 linetype 2 linecolor rgb "magenta"    linewidth 5.000 pointtype 3 pointsize default
-#set key at 5.2,1.5  noreverse enhanced box linetype -2 linewidth 1.200 samplen 2 spacing 1.5 width 0.0 height 0  
+#set key at 5.2,1.5  noreverse enhanced box linetype -2 linewidth 1.200 samplen 2 spacing 1.5 width 0.0 height 0
 set key right  top Right noreverse enhanced box linetype -2 linewidth 1.200 samplen 2 spacing 1.5 width 0.0 height 0  autotitles
 set xlabel 'K-K_0 [1/Bohr]' offset 0,0.5
 set ylabel 'Eigenvalue [Ha]' offset 1,0
@@ -155,18 +157,35 @@ set format x "%5.3f"
 set xtics rotate by -45
 #FIT_LIMIT= 1e-6
 #set fit quiet
+set fit logfile "$dirt-CB-fit.log"
 f1(x)= e * x**2 + f * x + g
-fit f1(x) "ke4fit-$dirt.dat" using 1:5 via e, f, g 
+fit f1(x) "ke4fit-$dirt.dat" using 1:5 via e, f, g
 #set datafile separator ","
 plot f1(x) with lines ls 4  notitle '',\\
-     "ke4fit-$dirt.dat" u 1:5   with points  lt 4 lw 4 pt 4 ps 4 notitle '' 
+     "ke4fit-$dirt.dat" u 1:5   with points  lt 4 lw 4 pt 4 ps 4 notitle ''
 !
 /usr/bin/gnuplot  plot-CB-ke-fit.plt
 
-e=`grep 'e               = ' fit.log | tail -1 | awk '{printf "%12.6f\n", $3}'`
-if [ ! -f EM-GNUPLOT.out  ]; then
-echo $a, $p, $u, $e  | awk '{printf "%12.3f %12.3f %12.3f %12.3f\n", 0.5/$1, 0.5/$2, 0.5/$3, 0.5/$4}' >EM-GNUPLOT.out
+e=`grep 'e         ' "$dirt"-CB-fit.log | grep '=' |tail -1 | awk '{printf "%12.6f\n", $3}'`
+if [ $i -eq  1  ]; then
+  echo "$dirt" > EM-GNUPLOT.out
 else
-echo $a, $p, $u, $e  | awk '{printf "%12.3f %12.3f %12.3f %12.3f\n", 0.5/$1, 0.5/$2, 0.5/$3, 0.5/$4}' >>EM-GNUPLOT.out
+  echo "$dirt" >> EM-GNUPLOT.out
 fi
+
+if [  ${#a} -eq 0  ];then
+a=10000
+fi
+if [  ${#p} -eq 0  ];then
+p=10000
+fi
+if [  ${#u} -eq 0  ];then
+u=10000
+fi
+if [  ${#e} -eq 0  ];then
+e=10000
+fi
+echo $a, $p, $u, $e  | awk '{printf "%12.3f %12.3f %12.3f %12.3f\n", 0.5/$1, 0.5/$2, 0.5/$3, 0.5/$4}' >>EM-GNUPLOT.out
 done
+echo "# 0.000 --> 'Singular matrix in Invert_RtR' in the fitting of gnuplot." >>EM-GNUPLOT.out
+echo "#Solution: manually fit it again using the command terminal of gnuplot instead of the script file." >>EM-GNUPLOT.out
